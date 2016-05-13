1190ff1281f0e7e8d193a0bd6d963847bbecb5a2
Joe Rimar <jrimar@jrimar-mbp15.local>
2016-05-13 09:32:58 -0400

    major update

--- local.scriptEngine.js
+++ local.scriptEngine.js
@@ -5,16 +5,13 @@
 
 
  $(document).ready(function() {
+
 // =============== Styling Scripts ===============
 
-$('td').children('img').css('max-width', '100%');
 
 
-        
-
-	$('#nav-icon3').click(function() {
+    $('#nav-icon3').click(function() {
 		$(this).toggleClass('open');
-		// $('#navBar').toggleClass('active');
 		$('#navBar').slideToggle(100);
 	});
 
@@ -24,9 +21,9 @@ $('td').children('img').css('max-width', '100%');
 		$(this).children('.openDownArrow').toggleClass('open');
 	});
 
-        $('.footerHiddenInfoItems').click(function(e) {
-                 e.stopPropagation();
-        });
+    $('.footerHiddenInfoItems').click(function(e) {
+             e.stopPropagation();
+    });
 
 	$('#weeklyHeading').click(function() {
 		$('#forcastCredit').toggleClass('inactive');
@@ -52,34 +49,34 @@ function getWeatherData() {
 	var forecastUrl = "https://api.forecast.io/forecast/";
 	var gK = revs('wiGXV','A9','tAgXKe','rW1UKoLY','CKorfF','640ZcCyS','azIA');
 	var fK = revs('0b595a','3a31ae239927405','70cf281eef9');
-	$.get(convert(googleUrl, gK, true, [107,101,121,61]), function(data) {
+	$.get(convert(googleUrl, false, true, [107,101,121,61]), function(data) {
 
 		//Check google response status
-		console.log(data.status);
+		//console.log(data.status);
 		//If nothing returned close weather div and stop request
 		if(data.status !== "OK") {
 			$("#weatherForcastOuter").toggleClass('inactive');
 			return null;
 		}
 		
-		console.log("Response Data: " + data.results);
+		//console.log("Response Data: " + data.results);
 		var lat = data.results[0].geometry.location.lat , lng = data.results[0].geometry.location.lng;
-		console.log(lat);
-		console.log(lng);
+		//console.log(lat);
+		//console.log(lng);
 
 		
 		//Ajax request to forecast.io api
 		$.ajax({
-			url:  convert(forecastUrl, fK) + '/' + lat + ',' + lng,
+			url:  convert(forecastUrl,true) + '/' + lat + ',' + lng,
 			dataType: 'jsonp'
 		}).error(function(err) {
-			console.log(err.statusText);
+			//console.log(err.statusText);
 			//Close weather div if request fails
 			$("#weatherForcastOuter").toggleClass('inactive');
 			
 		//Build Weather Divs for 7 Day Forcast
 		}).success(function(data) {
-			console.log(data);
+			//console.log(data);
 
 			//Div for current conditions
 			var currentConditions = document.getElementById('currentConditions');
@@ -108,11 +105,61 @@ function getWeatherData() {
 		});
 		//If goole Ajax Fails
 	}).fail(function(err) {
-		console.log(err.statusText);
+		//console.log(err.statusText);
 		//Close weather div if request fails
 		$("#weatherForcastOuter").toggleClass('inactive');
 	});
 }
+
+
+ $('.groupInfoBuyButton').click(function() {
+ 	$('body').animate({
+ 		scrollTop: $('.SecondaryColumnHeader').offset().top
+ 	}, 1000);
+ });
+
+
+
+
+/*function displayRotate() {
+	var table = document.getElementsByTagName('table');
+	var rotateDiv = document.getElementById('rotateDiv');
+	console.log(table);
+	console.log(changing);
+
+	if(table[0].width > 400) {
+		rotateDiv.className = 'showRotate';
+	} else {
+		rotateDiv.className = 'hideRotate';
+	}
+}
+
+function fixSizing() {
+	var tables = document.getElementsByTagName('table');
+	var outerDivWidth = document.getElementById('resultsInsert').offsetWidth;
+    console.log("Table Width: " + table.width);
+    console.log('Div width: ' + outerDivWidth);
+
+	tables[0].width = outerDivWidth - 20;
+	tables[1].width = 100;
+}*/
+
+
+
+	function changeRec(element) {
+		var child = element.children;
+
+ 		if(!child.length) return null; 
+
+ 		for(var i=0; i < child.length; i++) {
+ 			console.log(child[i]);
+ 			child[i].style = "";
+ 			child[i].removeAttribute('width');
+ 			child[i].removeAttribute('colspan');
+ 		
+ 			changeRec(child[i]);
+ 		}
+	}
 
 
  // =============== Helper Functions ===============	
@@ -144,7 +191,7 @@ function skyConditions(data) {
 //Used to get correct icon from meteocons.
 	//Not currently used...switched to cdn weather icons
 
-function getIcon(data) {
+/*function getIcon(data) {
 	switch (data) {
 		case 'clear-night':
 			return 'C';
@@ -168,7 +215,7 @@ function getIcon(data) {
 			return 'B';
 
 	}
-}
+}*/
 
 
 function formatDate(unixTime) {
@@ -178,22 +225,28 @@ function formatDate(unixTime) {
 }
 
 //Used to change input styles in results table
-function changeButtonColors(textColor, backgroundColor) {
+/*function changeButtonColors(textColor, backgroundColor) {
 	var buttons = document.querySelectorAll('input[type=button]');
 
 	for(var i = 0; i < buttons.length; i++) {
 		buttons[i].style.color = textColor;
 		buttons[i].style.backgroundColor = backgroundColor;
 	}
-}
+}*/
 
 
-// Followung two functions are used to mask the api keys to google maps and forecast.io
+// Following two functions are used to mask the api keys to google maps and forecast.io
 	//code purposely convoluded to make it harder to decipher
-function revs() {
+function revs(k) {
+	var args1 = ['wiGXV','A9','tAgXKe','rW1UKoLY','CKorfF','640ZcCyS','azIA'];
+	var args2 = ['0b595a','3a31ae239927405','70cf281eef9'];
+	var args = args1;
+
+	if(k) args = args2;
+
 	var tagString = '';
-	for(var i = 0; i < arguments.length; i++) {
-		tagString += arguments[i];
+	for(var i = 0; i < args.length; i++) {
+		tagString += args[i];
 		
 	}
 	var tagArr = tagString.split('').reverse();
@@ -202,19 +255,21 @@ function revs() {
 		
 		return (eCode +13).toString(2);
 			
-	}).join('/'); 	
+	});
 }
 
-function convert(url, nums, query,kCodeNums) {
+function convert(url, nums, q,kCodeNums) {
+	var num = ["1001110", "1010110", "10000111", "1101110", "1100000", "10000110", "1010000", "1110000", "1100111", "111101", "1000001", "1000011", "1010011", "1110011", "1111111", "1111100", "1011000", "1010000", "1100110", "1011001", "1111100", "1011000", "1100010", "111110", "1100100", "1111111", "1110010", "1011000", "1100101", "1110100", "1001110", "10000001", "1000110", "1001110", "1100011", "1100101", "1010100", "1110110", "10000100"];
+	if(nums) num = ["1000110", "1110011", "1110010", "1110010", "111110", "1000101", "111111", "1110011", "1110000", "111101", "1000100", "1000010", "111101", "1000001", "1000100", "111111", "1000110", "1000110", "1000000", "111111", "1110010", "1101110", "111110", "1000000", "1101110", "1000000", "1101110", "1000010", "1000110", "1000010", "1101111", "111101"];
  	var k = '';
- 	if(query && kCodeNums) k = (function() {
+ 	if(q && kCodeNums) k = (function() {
  		var c = '';
  		for (var i = 0; i < kCodeNums.length; i++) {
  			c += String.fromCharCode(kCodeNums[i]);
  		}
  		return c;
  	}());
-	return url + k + nums.split('/').map(function(e) {
+	return url + k + num.map(function(e) {
 		var bin = 1, dec=0, x;
 		for(var i = e.length-1; i >=0; i--) {
 			dec += e[i] * bin;
@@ -228,6 +283,15 @@ function convert(url, nums, query,kCodeNums) {
 function reverseIt(str) {
 	return str.split('').reverse().join("");;
 }
+
+function removeStyles(el) {
+	var el = document.getElementById(el), children = el.children;
+	el.removeChild(children[0]);
+}
+
+
+
+	
 
 
 //console.log(revs('https://maps.googleapis.com/maps/api/geocode/json?address=2633+Camino+Ramon,+San+Ramon,+CA&', convert('wiGXV','A9','tAgXKe','rW1UKoLY','CKorfF','640ZcCyS','azIA'), true, [107,101,121,61])); 
@@ -342,4 +406,7 @@ function formatDate(unixTime) {
 }
 
 */
+
+
+
  
